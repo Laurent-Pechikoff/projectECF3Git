44f29842a9a9cd4620a9965737e59df9642bd523
Laurent <laurent.pechikoff@gmail.com>
2021-02-01 16:31:52 +0100

    C1.4 Modification Application : ajout d'un sysou

--- src/appli/Application.java
+++ src/appli/Application.java
@@ -4,5 +4,6 @@ public class Application {
  public static void main(String[] args) {
 	System.out.println("coucou");
 	System.out.println("c'est nous");
+	System.out.println("c'est une vieille emission");
 }
 }
